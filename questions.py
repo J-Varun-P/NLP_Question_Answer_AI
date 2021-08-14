d456b944a8ef9a7b9366b84bf7f2b9f500207677
Varun <varunpawarjetling@gmail.com>
2021-08-14 22:46:08 +0530

    first commit

---
+++ questions.py
@@ -0,0 +1,120 @@
+import nltk
+import sys
+import os
+import string
+
+FILE_MATCHES = 1
+SENTENCE_MATCHES = 1
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python questions.py corpus")
+
+    # Calculate IDF values across files
+    files = load_files(sys.argv[1])
+    file_words = {
+        filename: tokenize(files[filename])
+        for filename in files
+    }
+    file_idfs = compute_idfs(file_words)
+
+    # Prompt user for query
+    query = set(tokenize(input("Query: ")))
+
+    # Determine top file matches according to TF-IDF
+    filenames = top_files(query, file_words, file_idfs, n=FILE_MATCHES)
+
+    # Extract sentences from top files
+    sentences = dict()
+    for filename in filenames:
+        for passage in files[filename].split("\n"):
+            for sentence in nltk.sent_tokenize(passage):
+                tokens = tokenize(sentence)
+                if tokens:
+                    sentences[sentence] = tokens
+
+    # Compute IDF values across sentences
+    idfs = compute_idfs(sentences)
+
+    # Determine top sentence matches
+    matches = top_sentences(query, sentences, idfs, n=SENTENCE_MATCHES)
+    for match in matches:
+        print(match)
+
+
+def load_files(directory):
+    """
+    Given a directory name, return a dictionary mapping the filename of each
+    `.txt` file inside that directory to the file's contents as a string.
+    """
+    dictionary = dict()
+    for x in os.listdir('corpus'):
+        file_path = os.path.join('corpus', x)
+        f = open(file_path, 'r', encoding="utf8")
+        dictionary[x] = f.read()
+        f.close()
+    return dictionary
+    #raise NotImplementedError
+
+
+def tokenize(document):
+    """
+    Given a document (represented as a string), return a list of all of the
+    words in that document, in order.
+
+    Process document by coverting all words to lowercase, and removing any
+    punctuation or English stopwords.
+    """
+    words = nltk.word_tokenize(document)
+    n = len(words)
+    for i in range(n):
+        words[i] = words[i].lower()
+    i = 0
+    x = string.punctuation
+    while i < n:
+        if words[i] in x:
+            words.remove(words[i])
+            i -= 1
+            n -= 1
+        i += 1
+    print(nltk.corpus.stopwords.words("english"))
+    #raise NotImplementedError
+
+
+def compute_idfs(documents):
+    """
+    Given a dictionary of `documents` that maps names of documents to a list
+    of words, return a dictionary that maps words to their IDF values.
+
+    Any word that appears in at least one of the documents should be in the
+    resulting dictionary.
+    """
+    raise NotImplementedError
+
+
+def top_files(query, files, idfs, n):
+    """
+    Given a `query` (a set of words), `files` (a dictionary mapping names of
+    files to a list of their words), and `idfs` (a dictionary mapping words
+    to their IDF values), return a list of the filenames of the the `n` top
+    files that match the query, ranked according to tf-idf.
+    """
+    raise NotImplementedError
+
+
+def top_sentences(query, sentences, idfs, n):
+    """
+    Given a `query` (a set of words), `sentences` (a dictionary mapping
+    sentences to a list of their words), and `idfs` (a dictionary mapping words
+    to their IDF values), return a list of the `n` top sentences that match
+    the query, ranked according to idf. If there are ties, preference should
+    be given to sentences that have a higher query term density.
+    """
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
